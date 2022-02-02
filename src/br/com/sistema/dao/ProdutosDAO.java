2544fd5d0b9ed19dca7d8ac9e53a412694563576
jeanpatussi <jean_patussi@hotmail.com>
2022-02-02 17:13:30 -0300

    Att_covid_vendas_persquisa_produto

--- src/br/com/sistema/dao/ProdutosDAO.java
+++ src/br/com/sistema/dao/ProdutosDAO.java
@@ -142,6 +142,42 @@ public class ProdutosDAO {
         }
 
     }
+    /*public List<Produtos> listarProdutos() {
+        try {
+
+            //1 passo criar a lista
+            List<Produtos> lista = new ArrayList<>();
+
+            //2 passo - criar o sql , organizar e executar.
+            String sql = "select p.id, p.descricao, p.preco, p.qtd_estoque, f.nome from tb_produtos as p "
+                    + "inner join tb_fornecedores as f on (p.for_id = f.id)";
+
+            PreparedStatement stmt = con.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Produtos obj = new Produtos();
+                Fornecedores f = new Fornecedores();
+                obj.setId(rs.getInt("p.id"));
+                obj.setDescricao(rs.getString("p.descricao"));
+                obj.setPreco(rs.getDouble("p.preco"));
+                obj.setQtd_estoque(rs.getInt("p.qtd_estoque"));
+
+                f.setNome(rs.getString(("f.nome")));
+
+                obj.setFornecedor(f);
+
+                lista.add(obj);
+            }
+
+            return lista;
+
+        } catch (SQLException erro) {
+
+            JOptionPane.showMessageDialog(null, "Erro :" + erro);
+            return null;
+        }
+
+    }*/
 
     //Metodo listar Produtos por Nome
     public List<Produtos> listarProdutosPorNome(String nome) {
@@ -169,6 +205,41 @@ public class ProdutosDAO {
                 f.setNome(rs.getString(("f.nome")));
 
                 obj.setFornecedor(f);
+
+                lista.add(obj);
+            }
+
+            return lista;
+
+        } catch (SQLException erro) {
+
+            JOptionPane.showMessageDialog(null, "Erro :" + erro);
+            return null;
+        }
+
+    }
+
+    //Metodo listar Produtos por Nome
+    public List<Produtos> listarProdutosPorDescricao(String nome) {
+        try {
+
+            //1 passo criar a lista
+            List<Produtos> lista = new ArrayList<>();
+
+            //2 passo - criar o sql , organizar e executar.
+            String sql = "select * from tb_produtos where descricao like ? ";
+
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, nome);
+
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Produtos obj = new Produtos();
+               
+                obj.setId(rs.getInt("id"));
+                obj.setDescricao(rs.getString("descricao"));
+                obj.setPreco(rs.getDouble("preco"));
+                obj.setQtd_estoque(rs.getInt("qtd_estoque"));
 
                 lista.add(obj);
             }
@@ -247,7 +318,34 @@ public class ProdutosDAO {
             return null;
         }
     }
+public Produtos buscaPorNome(String nome) {
+        try {
+            //1 passo - criar o sql , organizar e executar.
 
+            String sql = "select * from tb_produtos where descricao = ?";
+
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, nome);
+
+            ResultSet rs = stmt.executeQuery();
+            Produtos obj = new Produtos();
+
+            if (rs.next()) {
+
+                obj.setId(rs.getInt("id"));
+                obj.setDescricao(rs.getString("descricao"));
+                obj.setPreco(rs.getDouble("preco"));
+                obj.setQtd_estoque(rs.getInt("qtd_estoque"));
+
+            }
+
+            return obj;
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Produto não encontrado!");
+            return null;
+        }
+    }
     //Metodo  que da baixa no estoque
     public void baixaEstoque(int id, int qtd_nova) {
         try {
